2512fe5ced42dcccde823a549d6555bcf54da1d9
Narain-Singaram <112223598+Narain-Singaram@users.noreply.github.com>
2023-08-17 03:06:47 +0000

    what am i doing

--- SearchFilterBar.vue
+++ SearchFilterBar.vue
@@ -3,9 +3,16 @@
     <input
       v-model="searchQuery"
       @input="handleSearch"
+      @focus="showSuggestions = true"
+      @blur="showSuggestions = false"
       class="search-input"
       placeholder="Search partners"
     />
+    <ul v-if="showSuggestions" class="autocomplete-list">
+      <li v-for="suggestion in autocompleteSuggestions" :key="suggestion" @click="selectSuggestion(suggestion)">
+        {{ suggestion }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,34 +20,68 @@
 export default {
   data() {
     return {
-      searchQuery: ""
+      searchQuery: "",
+      showSuggestions: false,
+      partners: [
+    {
+      "id": 1,
+      "name": "Local Business A",
+      "type": "Business",
+      "contact": "John Doe"
+    },
+    {
+      "id": 2,
+      "name": "Community Organization B",
+      "type": "Community",
+      "contact": "Jane Smith"
+    }
+  ] // Your partner data array
     };
+  },
+  computed: {
+    autocompleteSuggestions() {
+      return this.partners
+        .filter(partner =>
+          partner.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+        )
+        .map(partner => partner.name);
+    }
   },
   methods: {
     handleSearch() {
       this.$emit("search", this.searchQuery);
+    },
+    selectSuggestion(suggestion) {
+      this.searchQuery = suggestion;
+      this.showSuggestions = false;
     }
   }
 };
 </script>
 
 <style scoped>
-.search-filter-bar {
-  margin-bottom: 20px;
-}
+/* ... Existing styles ... */
 
-.search-input {
-  padding: 10px;
+.autocomplete-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  position: absolute;
   width: 100%;
-  font-size: 1rem;
+  background-color: white;
   border: 1px solid #ddd;
-  border-radius: 5px;
-  transition: border-color 0.3s ease-in-out;
+  border-top: none;
+  border-radius: 0 0 5px 5px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 
-.search-input:focus {
-  outline: none;
-  border-color: #007bff;
-  box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+.autocomplete-list li {
+  padding: 5px 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.autocomplete-list li:hover {
+  background-color: #f5f5f5;
 }
 </style>
