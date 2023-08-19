cf313921f3fc261818e56923966c64a8142d9666
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-08-18 23:55:18 -0400

    add link support for grid and list view

--- components/PartnerList.vue
+++ components/PartnerList.vue
@@ -9,17 +9,21 @@
     <ul v-if="viewMode === 'grid'" class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
       <li v-for="partner in partners" :key="partner.id" class="bg-white rounded-xl shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] p-4">
         <!-- Content for Grid View -->
-        <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
-        <p class="text-gray-600 mt-2">Type: {{ partner.type }}</p>
-        <p class="text-gray-600">Contact: {{ partner.contact }}</p>
+        <NuxtLink :to="'/partner/' + partner.id">
+          <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
+          <p class="text-gray-600 mt-2">Type: {{ partner.type }}</p>
+          <p class="text-gray-600">Contact: {{ partner.contact }}</p>
+        </NuxtLink>
       </li>
     </ul>
     <ul v-else-if="viewMode === 'list'" class="space-y-4">
       <li v-for="partner in partners" :key="partner.id" class="bg-white rounded-lg shadow-md p-4">
         <!-- Content for List View -->
-        <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
-        <p class="text-gray-600">Type: {{ partner.type }}</p>
-        <p class="text-gray-600">Contact: {{ partner.contact }}</p>
+        <NuxtLink :to="'/partner/' + partner.id">
+          <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
+          <p class="text-gray-600">Type: {{ partner.type }}</p>
+          <p class="text-gray-600">Contact: {{ partner.contact }}</p>
+        </NuxtLink>
       </li>
     </ul>
     <ul v-else-if="viewMode === 'card'" class="space-y-4">
