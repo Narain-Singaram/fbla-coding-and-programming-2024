14746f7c31c9f6ad09678e7989d435698f408620
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-08-18 22:45:27 -0400

    Make partner card link to dedicated page.

--- components/PartnerList.vue
+++ components/PartnerList.vue
@@ -2,9 +2,11 @@
   <div class="bg-gray-100 p-6">
     <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
       <li v-for="partner in partners" :key="partner.id" class="bg-white rounded-lg shadow-md p-4">
-        <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
-        <p class="text-gray-600 mt-2">Type: {{ partner.type }}</p>
-        <p class="text-gray-600">Contact: {{ partner.contact }}</p>
+        <NuxtLink :to="'/partner/' + partner.id">
+          <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
+          <p class="text-gray-600 mt-2">Type: {{ partner.type }}</p>
+          <p class="text-gray-600">Contact: {{ partner.contact }}</p>
+        </NuxtLink>
       </li>
     </ul>
   </div>
@@ -12,8 +14,8 @@
 
 <script>
 export default {
-  props: {
-    partners: Array
-  }
+    props: {
+        partners: Array
+    },
 };
 </script>
