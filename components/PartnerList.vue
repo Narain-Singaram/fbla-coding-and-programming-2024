107d269ea236e96e237a5cbaec3b5ed228b3dee1
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2023-08-18 23:06:04 -0400

    use setup and typescript

--- components/PartnerList.vue
+++ components/PartnerList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-100 p-6">
     <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      <li v-for="partner in partners" :key="partner.id" class="bg-white rounded-lg shadow-md p-4">
+      <li v-for="partner in props.partners" :key="partner.id" class="bg-white rounded-lg shadow-md p-4">
         <NuxtLink :to="'/partner/' + partner.id">
           <h3 class="text-xl font-semibold">{{ partner.name }}</h3>
           <p class="text-gray-600 mt-2">Type: {{ partner.type }}</p>
@@ -12,10 +12,11 @@
   </div>
 </template>
 
-<script>
-export default {
-    props: {
-        partners: Array
-    },
-};
+<script setup lang="ts">
+const props = defineProps({
+  partners: {
+    type: Array<any>,
+    required: true,
+  },
+});
 </script>
